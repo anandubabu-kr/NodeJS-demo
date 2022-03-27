a94e0ca474eb608e58d6237f9ff3e306dc669b9f
AnanduBabucj7 <anandubabu.sd@gmail.com>
2022-03-27 20:40:51 +0530

    new file:   mongoDB/DOU.md - Document of understanding 	new file:   mongoDB/index.js - Basic set up for connecting to a mogoDB dtatabase

---
+++ mongoDB/DOU.md
@@ -0,0 +1,8 @@
+# Connecting to MongoDB
+
+1. Setup mongoDB
+2. Get the url for the cluster from 
+    > cluster --> connect --> connect application --> copy the url.
+3. In the code : define a connection URL.
+4. create a client connection object with forst paramenter as th URL and second parameter as the properties
+5. use mongo.connect() and specify connectivity.
---
+++ mongoDB/index.js
@@ -0,0 +1,9 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://anandubabu:<password>@cluster0.kkdvo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
